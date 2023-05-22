fe17efbd57c13547718e82777fad007713b93980
Sina Dabiri <sdabiri@emory.edu>
2023-05-22 18:12:49 -0400

    drafting p-value for all trials for one condition, 40Hz-AV.

--- trialsPerRF.m
+++ trialsPerRF.m
@@ -71,34 +71,35 @@ p_value_rows=matches(p_value_sig_40AV.Row,psd_results_soz_ch,'IgnoreCase',true);
 % check to see after how many trials their p-value became <0.05
 % 
 % evaluate_ent_degree_relpower_perTrial(fnames);
-conditions_of_interest=sort(PSD_results.condition(contains(PSD_results.condition,'Hz') & ~contains(PSD_results.condition,'occluded') & ~contains(PSD_results.condition,'min')));
+% conditions_of_interest=sort(PSD_results.condition(contains(PSD_results.condition,'Hz') & ~contains(PSD_results.condition,'occluded') & ~contains(PSD_results.condition,'min')));
 control_condition='Baseline';
-num_trials=size(PSD_results.data{rows,strcmp(PSD_results.condition,conditions_of_interest{i})}{1},1);
+conditions_of_interest = {'40Hz-AV'};
+num_trials=size(PSD_results.data{1,10}{1,1},1);
 zscore_table=zeros(length(PSD_results.label),length(conditions_of_interest));
 pvalue_table=zeros(length(PSD_results.label),length(conditions_of_interest));
 pvalue_trial_table=zeros(length(PSD_results.label),length(conditions_of_interest),length(num_trials));
-for i=1:size(zscore_table,2) %for each condition
-
-    freq_interest=str2num(regexprep(conditions_of_interest{i},'Hz.+',''));
-    [~,temp]=min(abs(PSD_results.data{1,strcmp(PSD_results.condition,conditions_of_interest{i})}{3}-freq_interest)); %find index of frequency closest to frequency of interest- have to do this because sample rate of EDF file not an integer sometimes (error with Natus)
-    freq_interest_index=temp;
+% for i=1:size(zscore_table,2) %for each condition
+% 
+%     freq_interest=str2num(regexprep(conditions_of_interest{i},'Hz.+',''));
+%     [~,temp]=min(abs(PSD_results.data{1,strcmp(PSD_results.condition,conditions_of_interest{i})}{3}-freq_interest)); %find index of frequency closest to frequency of interest- have to do this because sample rate of EDF file not an integer sometimes (error with Natus)
+%     freq_interest_index=temp;
     
     for ch=1:size(zscore_table,1) %for each channel
         stim_values=[];
         baseline_values=[];
         
-        for tr=1:size(PSD_results.data{ch,strcmp(PSD_results.condition,conditions_of_interest{i})}{1},1) %for however many number of trials of given condition there are
-            current_stim_value=PSD_results.data{ch,strcmp(PSD_results.condition,conditions_of_interest{i})}{1}(tr,freq_interest_index);
-            current_baseline_value=PSD_results.data{ch,strcmp(PSD_results.condition,control_condition)}{1}(tr,freq_interest_index);
+        for tr=1:num_trials %for however many number of trials of given condition there are
+            current_stim_value=PSD_results.data{ch,10}{1,1}(tr,:);
+            current_baseline_value=PSD_results.data{ch,2}{1,1}(tr,:);
 
             stim_values=[stim_values current_stim_value];
             baseline_values=[baseline_values current_baseline_value];
-            pvalue_trial_table(ch,i,tr)=pval_randomshuffle([stim_values' baseline_values'],10000);
+            pvalue_trial_table(ch,1,tr)=pval_randomshuffle([stim_values' baseline_values'],10000);
         end
-        zscore_table(ch,i)=(mean(stim_values)/mean(baseline_values))-1;
-        pvalue_table(ch,i)=pval_randomshuffle([stim_values' baseline_values'],10000);
+        zscore_table(ch,1)=(mean(stim_values)/mean(baseline_values))-1;
+        pvalue_table(ch,1)=pval_randomshuffle([stim_values' baseline_values'],10000);
     end
-end
+% end
 
 %         zscore_table=array2table(zscore_table,'RowNames',PSD_results.label,'VariableNames',conditions_of_interest); %make matrix into table
 %         writetable(zscore_table,[fnames.analysis_folder,'/LFP/static_ent/LFP_zscore_table_ref' ref_method{:} '.csv'],'WriteRowNames',1);
@@ -106,6 +107,6 @@ end
 %         pvalue_table=array2table(pvalue_table,'RowNames',PSD_results.label,'VariableNames',conditions_of_interest); %make matrix into table
 %         writetable(pvalue_table,[fnames.analysis_folder,'/LFP/static_ent/LFP_pvalue_table_ref' ref_method{:} '.csv'],'WriteRowNames',1);
 %        
-pvalue_trial_table=array2table(pvalue_trial_table,'RowNames',PSD_results.label,'VariableNames',conditions_of_interest); %make matrix into table
+pvalue_trial_table=array2table(pvalue_trial_table(:,1,:),'RowNames',PSD_results.label,'VariableNames',conditions_of_interest); %make matrix into table
 writetable(pvalue_trial_table,[fnames.analysis_folder,'/LFP/static_ent/LFP_pvalue_trial_table_ref' ref_method{:} '.csv'],'WriteRowNames',1);
        
